05af970b77557dc7d5d3b89af09e1869f3be9711
ArshadAQ <arshadunderscotch@gmail.com>
2019-03-25 00:32:28 +0530

    OOP basic model successfully done

--- pot_stepper_time1_firebase1_class/pot_stepper_time1_firebase1_class.ino
+++ pot_stepper_time1_firebase1_class/pot_stepper_time1_firebase1_class.ino
@@ -7,6 +7,9 @@
 #define WIFI_SSID "TP-Link_D322"
 #define WIFI_PASSWORD "aqaiyoom123"
 
+#define MAX_MODES 6
+#define MAX_BURNERS 6
+
 int potPin = A0;    // select the input pin for the potentiometer
 
 int val = 0;       // variable to store the value coming from the sensor
@@ -20,20 +23,20 @@ int pos = 0;
 const int stepPin = D8;
 const int dirPin = D7;
 
-String prev = "OFF";
-String next;
+//String prev = "OFF";
+//String next;
 
 int positions[] = {0, 45, 90, 135, 180};
 String modes[] = {"OFF", "ON-OFF", "ON", "ON-SIM", "SIM"};
 
 // stores object of duration for each mode for all burners
-FirebaseObject modesIntervalsNode[6] = {NULL, NULL, NULL, NULL, NULL, NULL};
-
-// array of ignition status of each burner
-boolean ignitionStatus[6];
-
-long startMillis[6] = {0};
-boolean start = true;
+//FirebaseObject modesIntervalsNode[6] = {NULL, NULL, NULL, NULL, NULL, NULL};
+//
+//// array of ignition status of each burner
+//boolean ignitionStatus[6];
+//
+//long startMillis[6] = {0};
+//boolean start = true;
 
 int num_burners;
 
@@ -41,54 +44,6 @@ int num_burners;
 int onEndTime = 15000;
 int simEndTime = 17000;
 
-void setup() {
-    
-    Serial.begin(9600);
-
-    // stepper motor pins
-    pinMode(stepPin, OUTPUT);
-    pinMode(dirPin, OUTPUT);
-
-    
-    // connect to wifi.
-    WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
-    Serial.print("connecting");
-    while (WiFi.status() != WL_CONNECTED) {
-        Serial.print(".");
-        delay(500);
-    }
-    
-    Serial.println();
-    Serial.print("connected: ");
-    Serial.println(WiFi.localIP());
-    
-    Firebase.begin(FIREBASE_HOST, FIREBASE_AUTH);
-    if(Firebase.success()){
-        Serial.println("connection made"); 
-    }
-    
-    String check;
-    
-    // wait until the app has initialised the user's information
-    do {
-        check = Firebase.getString("/aaa/model");
-    } while(Firebase.failed() || check.equalsIgnoreCase(""));
-
-    
-    num_burners = Firebase.getInt("/aaa/config/num_burners");
-    for(int i = 0; i < num_burners; i++){
-        ignitionStatus[i] = false;
-    }
-
-    
-    Firebase.stream("/aaa");
-    if(Firebase.success()){
-        Serial.println("streaming success"); 
-    } else {
-        Serial.println(Firebase.error());
-    }
-
-}
 
 int getDegree() {
     
@@ -179,12 +134,21 @@ class Burner{
     int burnerNumber;
     
     FirebaseObject modesIntervalsNode = NULL;
-    String modesOrder[];
-    float modesFinishTimes[];
+    String modesOrder[MAX_MODES];
+    float modesFinishTimes[MAX_MODES];
     int num_modes;
+
+    String knob_status;
+
+    // knob mode
+    // prev mode and the mode to switch to next
+    String prev, next;
 
     public:
 
+        // the path to the the burner object in firebase
+        String firebase_path;
+        
         int startMillis;
         StepperMotor motor;
         boolean ignited;
@@ -198,44 +162,69 @@ class Burner{
         void setIgnitionStatus(boolean state){
             this -> ignited = state;
         }
-        void initCooking(String path);
+        void initCooking();
         void autoKnobControl();
 
         boolean turnONandWaitForIgnition(String frstMode);
+
+        void setKnobStatus(String pos){
+            knob_status = pos;
+            Serial.print("knob status path is");
+            Serial.println(firebase_path + String("knob_status"));
+            Firebase.setString(firebase_path + String("knob_status"), pos);
+        }
 };
 
-void Burner :: initCooking(String path){
+void Burner :: initCooking(){
+
+    Serial.println("INit cooking");
 
     // time when cooking has started    
     startMillis = millis();
 
     // reflect the same in the database
-    Firebase.setBool(path + String("flame_detect"), true);
+    Serial.print("flame detect path is");
+    Serial.println(firebase_path + String("flame_detect"));
+    Firebase.setBool(firebase_path + String("flame_detect"), true);
     ignited = true;
     
     // initialise all modes
     JsonObject& modesIntervals = modesIntervalsNode.getJsonVariant();
     num_modes = modesIntervals.size();
+    Serial.print("Number of modes: ");
+    Serial.println(num_modes);
     
     int i = 0;
     float sum = 0;
     float val;
     for(JsonPair& kvp : modesIntervals) {
         // But the key is actually stored in RAM (in the JsonBuffer)
-        modesOrder[i++] = kvp.key;
+        String key = kvp.key;
+        modesOrder[i] = key;
         val = kvp.value;
         sum += val;
         modesFinishTimes[i++] = sum;
+//        Serial.println(key);
+//        Serial.println(sum);
+//        Serial.println(modesOrder[i-1]);
+//        Serial.println(modesFinishTimes[i-1]);
     }
+    
     
 }
 void Burner :: autoKnobControl(){
 
+    Serial.println("autoKnobControl called!");
     unsigned long diff = millis() - startMillis;
     for(int i = num_modes - 1; i >= 0; i--){
-        Serial.println(modesOrder[i]);
-        if(diff >= modesFinishTimes[i]){
+//        Serial.println(modesOrder[i]);
+//        Serial.println(modesFinishTimes[i]);
+        if(diff >= modesFinishTimes[i]*60000){
+            Serial.print(prev);
+            Serial.println("is over");
             next = (i == num_modes - 1 ? "OFF": modesOrder[i+1]);
+            Serial.print("Next mode is: ");
+            Serial.println(next);
             motor.setDirAndRotate(prev, next);
             prev = next;
             break;
@@ -244,13 +233,16 @@ void Burner :: autoKnobControl(){
 }
 
 boolean Burner :: turnONandWaitForIgnition(String frstMode){
+
+    Serial.println("turn on and wait");
     
     prev = "OFF";
     next = frstMode;
     motor.setDirAndRotate(prev, next);
+    setKnobStatus(next);
 
     // TODO: detect flame
-    boolean flame_detect = false;
+    boolean flame_detect = true;
     // check for flame 5 times within 10 seconds
     for(int i = 2000; i <= 10000; i+=2000){
         delay(i);
@@ -266,12 +258,14 @@ boolean Burner :: turnONandWaitForIgnition(String frstMode){
     }
 }
 
-Burner burners[6];
+Burner burners[MAX_BURNERS];
 
 void monitorManualKnob(int burnerNumber){
 
     // monitor manual knob(potentiometer) control
     deg = getDegree();
+
+    String prev, next;
     
     if (abs(prevDeg - deg) > 40) {
         // wait for 2 seconds and read again for a stable value
@@ -346,6 +340,56 @@ void monitorManualKnob(int burnerNumber){
     }
 }
 
+void setup() {
+    
+    Serial.begin(9600);
+
+    // stepper motor pins
+    pinMode(stepPin, OUTPUT);
+    pinMode(dirPin, OUTPUT);
+
+    
+    // connect to wifi.
+    WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+    Serial.print("connecting");
+    while (WiFi.status() != WL_CONNECTED) {
+        Serial.print(".");
+        delay(500);
+    }
+    
+    Serial.println();
+    Serial.print("connected: ");
+    Serial.println(WiFi.localIP());
+    
+    Firebase.begin(FIREBASE_HOST, FIREBASE_AUTH);
+    if(Firebase.success()){
+        Serial.println("connection made"); 
+    }
+    
+    String check;
+    
+    // wait until the app has initialised the user's information
+    do {
+        check = Firebase.getString("/aaa/model");
+    } while(Firebase.failed() || check.equalsIgnoreCase(""));
+
+    
+    num_burners = Firebase.getInt("/aaa/config/num_burners");
+    for(int i = 0; i < num_burners; i++){
+        burners[i].ignited = false;
+        burners[i].setModesIntervalsNode(NULL);
+    }
+
+    
+    Firebase.stream("/aaa");
+    if(Firebase.success()){
+        Serial.println("streaming success"); 
+    } else {
+        Serial.println(Firebase.error());
+    }
+
+}
+
 void loop() {
 
     // TODO: check for mass of vessel
@@ -355,6 +399,8 @@ void loop() {
         Burner curr_burner = burners[i];
         if(curr_burner.ignited){
 //            curr_burner.monitorManualKnob();
+//            Serial.print("calling auto knob control on burner");
+//            Serial.println(i);
             curr_burner.autoKnobControl();
         }
     }
@@ -394,15 +440,20 @@ void loop() {
 //                    String stepPath = String("/aaa/") + String("Food_profile/") + food_name + String("/steps");
 //                    Serial.println(stepPath);
 
-                    int burnerNumber = burner.substring(5).toInt() - 1;
+                    Serial.println(burner.substring(6));
+                    int burnerNumber = (burner.substring(6).toInt()) - 1;
+                    burners[burnerNumber].firebase_path = path;
+                    Serial.print("Burner number is: ");
                     Serial.println(burnerNumber);
-                    burners[burnerNumber].setModesIntervalsNode(Firebase.get(String("/aaa/") + String("Food_profile/") + food_name + String("/steps")));
+                    FirebaseObject modesIntervalsNode = Firebase.get(String("/aaa/") + String("Food_profile/") + food_name + String("/steps"));
+                    burners[burnerNumber].setModesIntervalsNode(modesIntervalsNode);
                     JsonObject& modesIntervals = (burners[burnerNumber].getModesIntervalsNode()).getJsonVariant();
                     
                     String frstMode;
                     for(JsonPair& kvp : modesIntervals) {
                         // But the key is actually stored in RAM (in the JsonBuffer)
                         frstMode = kvp.key;
+                        Serial.println(frstMode);
                         break;
                         
 //                        int val = kvp.value;
@@ -412,7 +463,7 @@ void loop() {
                     
                     boolean ignited = burners[burnerNumber].turnONandWaitForIgnition(frstMode);
                     if(ignited){
-                        burners[burnerNumber].initCooking(path);
+                        burners[burnerNumber].initCooking();
 //                        burners[burnerNumber].startMillis[burnerNumber] = millis();
 //                        Firebase.setBool(path + String("flame_detect"), true);
 //                        ignitionStatus[burnerNumber] = true;
